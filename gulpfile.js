f3ac3b40fc3caaa9aa2c57edeaf535f2741ceefa
Ammon Morris <likesalmon@gmail.com>
2015-03-28 11:04:17 -0700

    added views, browserify, and watch tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var watchify = require('watchify');
+var browserify = require('browserify');
 var mocha = require('gulp-mocha');
+
 var nodeFilesToWatch = ['app.js', 'api/**/*.js'];
 var nodeTestFiles = ['api/**/test/*.js'];
 
+/*
+    Api tasks
+*/
 gulp.task('mocha', function () {
     return gulp.src(nodeTestFiles, {read: false})
         .pipe(mocha({reporter: 'nyan'}));
@@ -12,4 +22,28 @@ gulp.task('mocha', function () {
 
 gulp.task('test', function () {
     gulp.watch(nodeFilesToWatch, ['mocha']);
+});
+
+/*
+    Client tasks
+*/
+gulp.task('browserify', function() {
+    var bundleStream = browserify({
+        entries: ['./client/main.js'],
+        debug: true
+    }).bundle().pipe(source('bundle.js'));
+    return bundleStream.pipe(gulp.dest('./public'));
+});
+
+gulp.task('views', function () {
+    gulp.src('client/index.html')
+        .pipe(gulp.dest('public/'));
+        
+    gulp.src('client/todo/*.html')
+        .pipe(gulp.dest('public/todo/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('client/**/*.js', ['browserify']);
+    gulp.watch(['client/**/*.html'], ['views']);
 });
