5452757538aaffe6d5eb78556495d36263f3fece
Ammon Morris <likesalmon@gmail.com>
2015-03-29 10:08:28 -0700

    implemented gulp sass watcher

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var buffer = require('vinyl-buffer');
 var watchify = require('watchify');
 var browserify = require('browserify');
 var mocha = require('gulp-mocha');
+var sass = require('gulp-sass');
 
 var nodeFilesToWatch = ['app.js', 'api/**/*.js'];
 var nodeTestFiles = ['api/**/test/*.js'];
@@ -38,12 +39,21 @@ gulp.task('browserify', function() {
 gulp.task('views', function () {
     gulp.src('client/index.html')
         .pipe(gulp.dest('public/'));
-        
+
     gulp.src('client/todo/*.html')
         .pipe(gulp.dest('public/todo/'));
+});
+
+gulp.task('sass', function () {
+    gulp.src('client/sass/*.scss')
+        .pipe(sass({ onError: function (err) {
+            console.error(err);
+        }}))
+        .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('watch', function () {
     gulp.watch('client/**/*.js', ['browserify']);
     gulp.watch(['client/**/*.html'], ['views']);
+    gulp.watch(['client/sass/*.scss'], ['sass']);
 });
