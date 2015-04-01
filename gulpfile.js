2340ae21ccbc11067cb3bd7737bfa4ca795b294e
Ammon Morris <likesalmon@gmail.com>
2015-04-01 08:03:36 -0700

    replaced vinyl packages with vinyl-transform

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 'use strict';
 
 var gulp = require('gulp');
-var gutil = require('gulp-util');
+// var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var watchify = require('watchify');
+// var source = require('vinyl-source-stream');
+// var buffer = require('vinyl-buffer');
+var transform = require('vinyl-transform');
+var rename = require('gulp-rename');
+// var watchify = require('watchify');
 var browserify = require('browserify');
 var mocha = require('gulp-mocha');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
-var minifyCSS = require('gulp-minify-css');
-var mochaPhantomjs = require('gulp-mocha-phantomjs');
+// var minifyCSS = require('gulp-minify-css');
+// var mochaPhantomjs = require('gulp-mocha-phantomjs');
 
 var nodeFilesToWatch = ['app.js', 'api/**/*.js'];
 var nodeTestFiles = ['api/**/test/*.js'];
@@ -33,25 +35,50 @@ gulp.task('lint-api', function() {
 });
 
 
-
-
 /*
     Client tasks
 */
 gulp.task('browserify', function() {
-    var bundleStream = browserify({
-        entries: ['./client/main.js'],
-        debug: true
-    }).bundle().pipe(source('bundle.js'));
-    return bundleStream.pipe(gulp.dest('./public'));
+    var bundle = transform(function(filename) {
+        var b = browserify({
+            entries: filename,
+            debug: true,
+            insertGlobals: true
+        });
+        return b.bundle();
+    });
+
+    return gulp.src(['./client/main.js'])
+        .pipe(bundle)
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(rename('bundle.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./public'))
+        .pipe(gulp.dest('./client/test'));
+    // var bundleStream = browserify({
+    //     entries: ['./client/main.js'],
+    //     debug: true
+    // }).bundle().pipe(source('bundle.js'));
+    // return bundleStream.pipe(gulp.dest('./public'));
 });
 
-gulp.task('testify', function() {
-    var bundleStream = browserify({
-        entries: ['./client/**/test/*.js'],
-        debug: true
-    }).bundle().pipe(source('test.bundle.js'));
-    return bundleStream.pipe(gulp.dest('./public'));
+gulp.task('browserify-tests', function() {
+    // var bundleStream = browserify({
+    //     entries: ['./client/todo/test/todo.ctrl.test.js'],
+    //     debug: true
+    // }).bundle().pipe(source('test.bundle.js'));
+    // return bundleStream.pipe(gulp.dest('./client/test/'));
+    var browserified = transform(function(filename) {
+        var b = browserify(filename);
+        return b.bundle();
+    });
+
+    // hello gulp.src() my old friend
+    return gulp.src(['./client/**/test/*.js'])
+        .pipe(browserified)
+        .pipe(rename('test.bundle.js'))
+        .pipe(gulp.dest('./client/test'));
 });
 
 gulp.task('views', function () {
@@ -72,7 +99,7 @@ gulp.task('sass', function () {
 });
 
 gulp.task('lint-client', function() {
-    return gulp.src('./client/**/*.js')
+    return gulp.src(['client/**/*.js', '!client/test/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
@@ -83,7 +110,7 @@ gulp.task('lint-client', function() {
 */
 gulp.task('watch', function () {
     gulp.watch(['app.js', 'api/**/*.js'], ['lint-api']);
-    gulp.watch('client/**/*.js', ['lint-client', 'browserify']);
+    gulp.watch(['client/**/*.js', '!client/test/*.js'], ['lint-client', 'browserify']);
     gulp.watch(['client/**/*.html'], ['views']);
     gulp.watch(['client/sass/*.scss'], ['sass']);
 });
