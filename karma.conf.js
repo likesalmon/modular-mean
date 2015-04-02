9b6ca14b7c0759b8b795b6c6cd3484d0d80e53ec
Ammon Morris <likesalmon@gmail.com>
2015-04-02 09:04:14 -0700

    added mocha reporter

--- karma.conf.js
+++ karma.conf.js
@@ -16,8 +16,8 @@ module.exports = function(config) {
 
         plugins : [
             'karma-mocha',
+            'karma-mocha-reporter',
             'karma-browserify',
-            'karma-jasmine',
             'karma-phantomjs-launcher',
             'karma-coverage',
             'karma-chai',
@@ -44,7 +44,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'spec'],
+        reporters: ['mocha'],
 
         browserify: {
             debug: true,
