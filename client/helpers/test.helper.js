1eade8263b4f9b553dfc4b3bfa3caba99b373066
Ammon Morris <likesalmon@gmail.com>
2015-04-08 08:40:04 -0700

    cleanup

--- client/helpers/test.helper.js
+++ client/helpers/test.helper.js
@@ -3,25 +3,28 @@
 // Load in our actual project
 // require('MyApp');
 //
-// // Dependencies
+// Dependencies
 // require('angular-mocks');
-// var chai = require('chai');
-// chai.use('sinon-chai');
-// chai.use('chai-as-promised');
-//
-// var sinon = require('sinon');
-//
-// beforeEach(function() {
-//   // Create a new sandbox before each test
-//   this.sinon = sinon.sandbox.create();
-// });
-//
-// afterEach(function() {
-//   // Cleanup the sandbox to remove all the stubs
-//   this.sinon.restore();
-// });
-//
-// module.exports = {
-//   rootUrl: 'http://localhost:3000',
-//   expect: chai.expect
-// };
+var chai = require('chai');
+var sinon = require('sinon');
+var chaiAsPromised = require('chai-as-promised');
+var sinonChai = require('sinon-chai');
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+
+
+beforeEach(function() {
+    // Create a new sandbox before each test
+    this.sinon = sinon.sandbox.create();
+});
+
+afterEach(function() {
+    // Cleanup the sandbox to remove all the stubs
+    this.sinon.restore();
+});
+
+module.exports = {
+    rootUrl: 'http://localhost:3000',
+    expect: chai.expect
+};
