bdbdb1a1a2c052f9abf4a6d7d4cd1bd64c0b363c
Ammon Morris <likesalmon@gmail.com>
2015-04-08 08:42:04 -0700

    refactored todo.page.js

--- client/todo/test/todo.page.js
+++ client/todo/test/todo.page.js
@@ -1,24 +1,13 @@
 'use strict';
 
-var expect = require('chai').expect;
+var helper = require('../../helpers/test.helper.js');
 
-describe('test setup', function() {
-    it('todo.page.js should work', function() {
-        expect(true).to.be.true;
-    });
-});
+function ToDoPage () {
+    this.get = function() {
+        browser.get(helper.rootUrl + '/');
+    };
 
-// var helpers = require('../../test/test.helper.js');
-//
-// module.exports = function () {
-//     this.get = function() {
-//         browser.get(helpers.rootUrl + '/widgets');
-//     };
-//
-//     this.widgetRepeater = by.repeater('widget in widgets');
-//     this.firstWidget = element(this.widgetRepeater.row(0));
-//
-//     this.widgetCreateForm = element(by.css('.widget-create-form'));
-//     this.widgetCreateNameField = this.widgetCreateForm.element(by.model('widget.name'));
-//     this.widgetCreateSubmit = this.widgetCreateForm.element(by.buttonText('Create'));
-// };
+    this.foo = element(by.binding('foo'));
+}
+
+module.exports = ToDoPage;
