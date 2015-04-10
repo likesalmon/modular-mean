461c5aeb3444643586ab172194f417449631938c
Ammon Morris <likesalmon@gmail.com>
2015-04-10 05:56:21 -0700

    added .otherwise() redirect

--- client/main.js
+++ client/main.js
@@ -1,11 +1,12 @@
 'use strict';
 
 require('angular');
+var _ = require('lodash');
 
 var uiRoute = require('angular-ui-router');
 var app = angular.module('MyApp', [uiRoute]);
 
-app.config(['$locationProvider', '$stateProvider', function($locationProvider, $stateProvider) {
+app.config(['$locationProvider', '$stateProvider','$urlRouterProvider', function($locationProvider, $stateProvider, $urlRouterProvider) {
     $locationProvider.html5Mode(true);
 
     $stateProvider
@@ -24,6 +25,24 @@ app.config(['$locationProvider', '$stateProvider', function($locationProvider, $
             controller: require('./todo/todo.ctrl.js').inject(app),
             templateUrl: './todo/todo.html'
         });
+
+        $urlRouterProvider.otherwise('/');
 }]);
 
+// app.run(['$state', '$rootScope', function ($state, $rootScope) {
+//     // var authNotRequired = ['/login', '/todo'];
+//     //
+//     // var authIsRequired = function (route) {
+//     //     _.find(authNotRequired, function (noAuthRoute) {
+//     //         return _.str.startsWith(route, noAuthRoute);
+//     //     });
+//     // };
+//     // debugger
+//     $rootScope.on('$routeChangeStart', function () {
+//         // if (authIsRequired) {
+//         //     $state.go('dashboard');
+//         // }
+//         console.log($state.current);
+//     });
+// }]);
 app.run();
