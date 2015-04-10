bf90c8ebddc9dbaf37fba095c9a6bc3da82b24d9
Ammon Morris <likesalmon@gmail.com>
2015-04-10 05:59:07 -0700

    made test pass"

--- client/todo/test/todo.new.page.js
+++ client/todo/test/todo.new.page.js
@@ -4,6 +4,6 @@ var expect = require('chai').expect;
 
 describe('test setup', function() {
     it('todo.new.page.js should work', function() {
-        expect(true).to.be.false;
+        expect(true).to.be.true;
     });
 });
