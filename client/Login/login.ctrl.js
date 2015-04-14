30c9362507c4b71576d5ec83ce229e92338ad514
Ammon Morris <likesalmon@gmail.com>
2015-04-14 09:29:59 -0700

    capitalized angular scripts

--- client/Login/login.ctrl.js
+++ client/Login/login.ctrl.js
@@ -1,10 +1,5 @@
 'use strict';
 
-exports.inject = function(app) {
-    app.controller('LoginCtrl', exports.controller);
-    return exports.controller;
-};
-
-exports.controller = ['$scope', function ($scope) {
+module.exports = ['$scope', function ($scope) {
     $scope.foo = 'bar';
 }];
