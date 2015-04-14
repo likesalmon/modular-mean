5c45366eaeabab836ebb1fdd6fb2ba83b0954350
Ammon Morris <likesalmon@gmail.com>
2015-04-14 09:45:58 -0700

    cleanup after merge conflict

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 'use strict';
 
 var express = require('express');
-var path = require('path');
 // var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
-
-/*
-    Local modules
-*/
-
-var todo = require('./api/to-do/to-do');
 
 
 /*
