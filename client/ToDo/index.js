06560079c68f3535b5c55e5c16c9ea564cfbad52
Ammon Morris <likesalmon@gmail.com>
2015-04-20 09:47:59 -0700

    injected ngMaterial

--- client/ToDo/index.js
+++ client/ToDo/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var angular = require('angular');
+var angularMaterial = require('angular-material');
+var angularAnimate = require('angular-animate');
+var angularAria = require('angular-aria');
 var confirmButtonDirective = require('./ToDoConfirmButtonDirective');
 
-module.exports = angular.module('ToDo', [])
+module.exports = angular.module('ToDo', ['ngMaterial', 'ngAnimate', 'ngAria'])
     .directive('confirmButton', require('./ToDoConfirmButtonDirective.js'));
