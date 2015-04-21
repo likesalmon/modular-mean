66f3bd3b68ca3ee4a6863c02e8341f5907105519
Ammon Morris <likesalmon@gmail.com>
2015-04-21 06:17:09 -0700

    added todos

--- client/ToDo/ToDoController.js
+++ client/ToDo/ToDoController.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = ['$scope', function ($scope) {
-    $scope.foo = 'bar';
+    $scope.todos = [
+        {
+            title: 'First todo',
+            description: '<p>This is the first thing to do.</p>'
+        },
+        {
+            title: 'Second todo',
+            description: '<p>This is the second thing to do.</p>'
+        }
+    ];
 }];
