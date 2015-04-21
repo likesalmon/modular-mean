88b694afa9e1e4e4a8e2cc105692e973929ad68a
Ammon Morris <likesalmon@gmail.com>
2015-04-21 09:13:38 -0700

    registered ToDoService

--- client/ToDo/index.js
+++ client/ToDo/index.js
@@ -4,8 +4,8 @@ var angular = require('angular');
 var angularMaterial = require('angular-material');
 var angularAnimate = require('angular-animate');
 var angularAria = require('angular-aria');
-var confirmButtonDirective = require('./ToDoConfirmButtonDirective');
 
 module.exports = angular.module('ToDo', ['ngMaterial', 'ngAnimate', 'ngAria'])
     .directive('confirmButton', require('./ToDoConfirmButtonDirective'))
-    .controller('ToDoController', require('./ToDoController'));
+    .controller('ToDoController', require('./ToDoController'))
+    .factory('ToDoService', require('./ToDoService'));
