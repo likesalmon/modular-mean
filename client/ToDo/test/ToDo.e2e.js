b0eacee572a6d6386e676501c74a4124c91a4ed4
Ammon Morris <likesalmon@gmail.com>
2015-04-21 06:45:52 -0700

    added tests for title, button, repeater

--- client/ToDo/test/ToDo.e2e.js
+++ client/ToDo/test/ToDo.e2e.js
@@ -4,13 +4,34 @@ var helper = require('../../helpers/test.helper.js');
 var expect = helper.expect;
 var ToDoPage = require('./ToDo.page');
 
-describe('todo page', function() {
+describe('todo landing page', function() {
     beforeEach(function () {
         this.page = new ToDoPage();
         this.page.get();
     });
 
-    it('should have a binding to foo', function() {
-        expect(this.page.foo.getText()).to.eventually.equal('bar');
+    describe('title', function () {
+        it('should have a title', function () {
+            expect(this.page.title.getText()).to.eventually.equal('To Do');
+        });
+    });
+
+    describe('new button', function () {
+        it('should have a button that creates a new todo', function () {
+            expect(this.page.newButton.getText()).to.eventually.equal('New');
+        });
+    });
+
+    describe('todos', function () {
+        it('should start with 2 todos', function () {
+            expect(this.page.todoList.count()).to.eventually.equal(2);
+        });
+
+        it('should have a title and description', function () {
+            this.page.todoList.each(function (el, index) {
+                expect(el.$('h3').isPresent()).to.eventually.equal(true);
+                expect(el.$('.description').isPresent()).to.eventually.equal(true);
+            });
+        });
     });
 });
