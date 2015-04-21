d543638b4d0a19b37d69257576f42752679fc458
Ammon Morris <likesalmon@gmail.com>
2015-04-21 06:21:34 -0700

    no longer using angular as a dependency

--- client/ToDo/test/ToDoController.test.js
+++ client/ToDo/test/ToDoController.test.js
@@ -1,9 +1,19 @@
 'use strict';
 
 var expect = require('chai').expect;
+var ToDoController = require('../ToDoController');
 
-describe('test setup', function() {
-    it('ToDoController.test.js should work', function() {
-        expect(true).to.be.true;
+describe('ToDoController', function() {
+    var scope,
+        ctrl;
+
+    beforeEach(function () {
+        scope = {};
+        ctrl = new ToDoController[1](scope);
+    });
+
+    it('ToDoController should exist', function() {
+        expect(!!ctrl).to.equal(true);
+        expect(scope.todos).to.be.an('array');
     });
 });
