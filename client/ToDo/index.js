21043873aa708ab98670153eca31b3dcc4b5b54e
Ammon Morris <likesalmon@gmail.com>
2015-04-30 14:14:54 -0700

    added ngMdIcons dependency

--- client/ToDo/index.js
+++ client/ToDo/index.js
@@ -4,8 +4,9 @@ var angular = require('angular');
 var angularMaterial = require('angular-material');
 var angularAnimate = require('angular-animate');
 var angularAria = require('angular-aria');
+require('../../node_modules/angular-material-icons/angular-material-icons');
 
-module.exports = angular.module('ToDo', ['ngMaterial', 'ngAnimate', 'ngAria'])
+module.exports = angular.module('ToDo', ['ngMaterial', 'ngAnimate', 'ngAria', 'ngMdIcons'])
     .directive('confirmButton', require('./ToDoConfirmButtonDirective'))
     .controller('ToDoController', require('./ToDoController'))
     .factory('ToDoService', require('./ToDoService'));
