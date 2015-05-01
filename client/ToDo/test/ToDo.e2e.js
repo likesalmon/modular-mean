1b25bd9cd758f921926064421800eb1063656083
Ammon Morris <likesalmon@gmail.com>
2015-05-01 09:37:16 -0700

    refactored in revealing module style

--- client/ToDo/test/ToDo.e2e.js
+++ client/ToDo/test/ToDo.e2e.js
@@ -5,30 +5,20 @@ var expect = helper.expect;
 var ToDoPage = require('./ToDo.page');
 
 describe('todo landing page', function() {
+    var page;
+
     beforeEach(function () {
-        this.page = new ToDoPage();
-        this.page.get();
-    });
-
-    describe('title', function () {
-        it('should have a title', function () {
-            expect(this.page.banner.getText()).to.eventually.equal('To Do');
-        });
-    });
-
-    describe('new button', function () {
-        it('should have a button that creates a new todo', function () {
-            expect(this.page.newButton.getText()).to.eventually.equal('+');
-        });
+        page = new ToDoPage();
+        page.get();
     });
 
     describe('todos', function () {
         it('should start with 2 todos', function () {
-            expect(this.page.todoList.count()).to.eventually.equal(2);
+            expect(page.todoList.count()).to.eventually.equal(2);
         });
 
         it('should have a title and description', function () {
-            this.page.todoList.each(function (el, index) {
+            page.todoList.each(function (el, index) {
                 expect(el.$('button').$('h3').isPresent()).to.eventually.equal(true);
             });
         });
