bab4306a330e251243e2eebe33dcdbf0d96c5de2
Ammon Morris <likesalmon@gmail.com>
2015-05-05 11:39:06 -0700

    removed select() method

--- client/ToDo/ToDoController.js
+++ client/ToDo/ToDoController.js
@@ -2,14 +2,4 @@
 
 module.exports = ['$scope', 'ToDoService', function ($scope, ToDoService) {
     $scope.todos = ToDoService.all();
-
-    $scope.select = function (item) {
-        $scope.todos.forEach(function (todo) {
-            todo.selected = false;
-        });
-        item.selected = true;
-        
-        $scope.selected = item;
-    };
-
 }];
