fc301e81903b02af5f81879939a2976fb597a180
Ammon Morris <likesalmon@gmail.com>
2015-05-07 06:57:21 -0700

    added page elements"

--- client/ToDo/test/ToDoNew.page.js
+++ client/ToDo/test/ToDoNew.page.js
@@ -1,9 +1,41 @@
 'use strict';
 
-var expect = require('chai').expect;
+var helper = require('../../helpers/test.helper.js');
 
-describe('test setup', function() {
-    it('todo.new.page.js should work', function() {
-        expect(true).to.be.true;
-    });
-});
+module.exports = function () {
+    var get = function() {
+        browser.get(helper.rootUrl + '/todo/new');
+    };
+
+    var content = element(by.css('#content'));
+    var title = content.$('.title');
+    var form = element(by.css('#newTodoForm'));
+    var titleInput = element(by.model('newTodo.title'));
+    var descriptionTextarea = element(by.model('newTodo.description'));
+    var saveButton = element(by.css('#save-button'));
+
+    var addContentToTitleInput = function (content) {
+        return titleInput.sendKeys(content);
+    };
+
+    var addContentToTextarea = function (content) {
+        return descriptionTextarea.sendKeys(content);
+    };
+
+    var clickSaveButton = function () {
+        return saveButton.click();
+    };
+
+    return {
+        get: get,
+        content: content,
+        title: title,
+        form: form,
+        titleInput: titleInput,
+        descriptionTextarea: descriptionTextarea,
+        saveButton: saveButton,
+        addContentToTitleInput: addContentToTitleInput,
+        addContentToTextarea: addContentToTextarea,
+        clickSaveButton: clickSaveButton
+    };
+};
